cf3b54379a9500eb2f1456a35d9d8906b5042b7a
Matheus Cerqueira <matheuscbv23@gmail.com>
2024-03-11 18:07:46 -0300

    Atualização

--- Curso_Udemy/04-165-funcoes_decoradores_e_decoradores/decoradores_com_parametros.py
+++ Curso_Udemy/04-165-funcoes_decoradores_e_decoradores/decoradores_com_parametros.py
@@ -2,8 +2,8 @@
 # com o auxílio de uma função externa a nossa função decoradora. Veja a seguir:
 
 # O valor pode ser utilizado em qualquer escopo interno da funcao_recebe_parametro
-def funcao_recebe_parametro(valor):
-    def funcao_decoradora(funcao):
+def funcao_recebe_parametro(valor):  # Responsável por retornar decoradores
+    def funcao_decoradora(funcao):  # Responsável por retornar funções
         def closure():
             print('Execução da closure que irá executar a função decorada.')
             resultado = funcao()
@@ -15,7 +15,9 @@ def funcao_recebe_parametro(valor):
 
 # Ao utilizar uma função externa como decorador, podemos estar passando parâmetros
 # que podem ser utilizados pelas demais funções interna a qualquer momento.
-# Tal decorador irá retornar nossa função decoradora (funcao_decoradora) logo em seguida.
+# Tal decorador irá retornar nossa função decoradora (funcao_decoradora), que irá receber a
+# função funcao_decorada por parâmetro, retornando nossa função interna (closure). Isso seria equivalente a:
+# executa = funcao_recebe_parametro('valor qualquer')(funcao_decorada).
 # Obs.: É possível termos mais de um decorador para uma mesma função.
 # A ordem de execução dos decoradores será sempre do mais próximo da função para cima.
 # funcao_recebe_parametro('mais um valor') - Esse decorador seria o segundo a ser executado.
@@ -25,10 +27,6 @@ def funcao_decorada():
     return 'Eu sou a função decorada e estou sendo executada...'
 
 
-# Para que nossa funcão interna (closure) seja retornada,
-# devemos nos referenciar a própria função funcao_recebe_parametro, passando por parâmetro
-# determinado valor, e por fim, a função que queremos que seja decorada. Essa será utilizada
-# pela função decoradora que será retornada.
-executa = funcao_recebe_parametro('outro valor')(funcao_decorada)
+executa = funcao_decorada
 # Por último, iremos chamar nosso closure, que irá nos exibir o resultado.
 print(executa())
