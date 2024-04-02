8216aadb6548e183c484679ff86a92655ca52fea
Matheus Cerqueira <matheuscbv23@gmail.com>
2024-04-02 00:06:50 -0300

    Atualização

---
+++ Curso_Udemy/05-236-dunder_methods_ou_special_methods/__new__e__init__.py
@@ -0,0 +1,38 @@
+# Os métodos especiais __new__ e __init__ são responsáveis por criar e retornar um novo objeto e
+# inicializar uma instância, respectivamente.
+
+# Em nosso método __new__, devemos passar a referência a classe (cls), já que será responsável por criar
+# a instância referente à aquela classe.
+#
+# Um método __new__ deve retornar um objeto, enquanto __init__ não deve retornar nada (None).
+
+# Nossas classes herdam por padrão da classe object. Essa é uma classe mais ao topo, sendo responsável
+# pela criação de nossas instâncias.
+class NossaClasse:
+
+    # O método new costuma não ser utilizado geralmente, porém, uma de suas possíveis
+    # utilidades, é a execução de ações antes ou depois da criação do nosso objeto.
+    # Por padrão, passamos args e kwargs para não nos preocuparmos com a quantidade de parâmetros recebidos.
+    def __new__(cls, *args, **kwargs):
+        print('Mensagem antes de criar a instância.')
+        # É recomendado utilizarmos o super() ao invés de object ao criarmos nossas instâncias em __new__.
+        instancia = super().__new__(cls)
+        print('Mensagem depois de criar a instância.')
+        # Podemos inclusive criar atributos para nossa instância. Dessa forma, todas as instâncias criadas
+        # terão esse atributo:
+        instancia.valor = 'texto qualquer'
+        # Retornando nosso objeto:
+        return instancia
+
+    def __init__(self, x):
+        self.x = x
+        print('__init__ está sendo acionado...')
+
+
+# Quando criamos uma instância, os seguintes passos são executados:
+# obj = object.__new__(NossaClasse)
+# obj.__init__()
+# print(obj)
+
+obj = NossaClasse(11)
+print(obj.valor)
---
+++ Curso_Udemy/05-236-dunder_methods_ou_special_methods/__repr__e__str__.py
@@ -0,0 +1,33 @@
+class Coordenadas:
+    def __init__(self, x, y, z='Referêcia'):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    # Com o método especial __repr__, definimos a representação de nossos objetos. Com ele, informamos a
+    # estrutura de nosso objeto, como uma forma de documentação para quando for exibido. Esse método é
+    # muito utilizado para auxiliar outros desenvolvedores ao trabalharem com o código.
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        # É aconselhado utilizarmos f strings com a especificação para o tipo __repr__.
+        return f'{class_name}(x={self.x!r}, y={self.y!r}, z={self.z!r})'
+        # Como podemos ver, notamos diferença no parâmetro z. Como ele é do tipo string, aspas foram
+        # adicionadas em sua formatação.
+
+    # Com o método especial __str__, definiremos o que será retornado quando nosso objeto for requisitado
+    # em formato de string. Por padrão, o método __str__ é utilizado sempre que definido. Caso contrário, é feito um
+    # fallback, direcionando a execução para o método __repr__, caso tenha sido definido.
+    # Obs.: fallback é uma abordagem proativa para lidar com possíveis falhas ou problemas, garantindo que haja uma
+    # solução alternativa ou plano de contingência disponível.
+    def __str__(self):
+        return f'(x={self.x}, y={self.y})'
+
+
+coordenada_01 = Coordenadas(123, 321)
+print(coordenada_01)
+# Mesmo com um método __str__ definido, ainda conseguimos chamar nosso método __repr__:
+# Primeira opção:
+print(repr(coordenada_01))
+# Segunda opção:
+print(f'{coordenada_01!r}')
+# No caso acima, !s significa __string__ e !r __repr__.
