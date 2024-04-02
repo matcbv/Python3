5f095dc95ff5dd91379b63938cc7afa1bcdbcd74
Matheus Cerqueira <matheuscbv23@gmail.com>
2024-04-02 18:55:33 -0300

    Atualização

--- Curso_Udemy/teste.py
+++ Curso_Udemy/teste.py
@@ -1,3 +1,17 @@
-class A:
+from abc import ABC
+
+class PostoGasolina(ABC):
     def __init__(self):
-        self.classe
+        self.litros_comprados
+        self.litros_comprados
+        self.valor
+        self.litros_disponiveis
+
+    @abstractmethod
+    def reabastecer_bomba:
+
+
+class Bomba01:
+    def __init__(self):
+        super().__init__()
+        self.carro = carro
