2d5dfd08dd7ada1f72e5f7c20bb1e2437f7d24d0
Matheus Cerqueira <matheuscbv23@gmail.com>
2024-04-10 15:44:43 -0300

    Update criando_nossa_funcao_decoradora.py

--- Curso_Udemy/05-246-classes_decoradoras/criando_nossa_funcao_decoradora.py
+++ Curso_Udemy/05-246-classes_decoradoras/criando_nossa_funcao_decoradora.py
@@ -1,5 +1,6 @@
-# Quando utilizamos uma classe como decoradora, o método a ser decorado passa a ser uma
-# instância dessa classe, e a função se torna um atributo dessa instância.
+# Quando utilizamos uma classe como decoradora, o método/função a ser decorado,
+# passa a ser uma instância dessa classe, e o verdadeiro método/função se torna um
+# atributo dessa instância (no exemplo abaixo, o atributo func).
 class Somar:
     def __init__(self, func):
         # func passa a ser a função soma.
@@ -13,9 +14,14 @@ class Somar:
 
 
 @Somar
+# soma passa a ser uma instância de Somar.
 def soma(x, y):
     return x + y
 
 
+# Quando chamamos soma, estamos chamando uma instância da classe Somar.
+# Caso não tivéssemos implementado o método __call__, teríamos que chamar nossa
+# função da seguinte maneira: soma.func(2, 2). Entretanto, __call__ torna nossa
+# instância chamável.
 resultado = soma(2, 2)
 print(resultado)
