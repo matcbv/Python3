82cd4b8ff24cfb60f12b47cec175e2edff45c28b
Matheus Cerqueira <matheuscbv23@gmail.com>
2024-04-11 15:18:41 -0300

    Update __init__.py

--- Curso_Udemy/06-279-calendar/__init__.py
+++ Curso_Udemy/06-279-calendar/__init__.py
@@ -6,21 +6,23 @@ import calendar
 print(calendar.calendar(2024))
 print('-'*90)
 
-# Podemos também obter o calendário de um mês em específico com a função monthcalendar:
-print(calendar.monthcalendar(2024, 12))
+# Podemos descobrir o índice do dia da semana e o último dia do mês, através do comando monthrange:
+indice_semana, ultimo_dia_mes = calendar.monthrange(2024, 12)
+# O índice dos dias da semana começam em 0, representando segunda, e vão até 6, representando domingo.
+print(list(enumerate(calendar.day_name)))
 print('-'*90)
-
-# Podemos descobrir o índice do dia semana e o último dia do mês através do comando monthrange:
-# O índice dos dias da semana começa em 0, representando a segunda, e vai até 6, representando o domingo.
-print(calendar.monthrange(2024, 12))
-print('-'*90)
-
-# Através do comando weekday, descobrimos o índice da semana que determinado dia possui.
-indice_semana = calendar.weekday(2024, 12, 5)
-print(indice_semana)
 
 # Para descobrirmos o nome do dia da semana, utilizamos o comando day_name. Por parâmetro, passamos o
 # índice da semana obtido.
-print(calendar.day_name[indice_semana])
+# Através do comando weekday, descobrimos o índice da semana que determinado dia possui.
+print(calendar.day_name[calendar.weekday(2024, 12, 5)])
 
-# Podemos criar um calendár
+# Podemos também, obter o calendário de um mês em específico com a função monthcalendar.
+# Tal função irá retornar uma lista contendo os dias do mês, organizados em listas que represetam
+# as semanas daquele mês.
+# Obs.: Os dias com valor 0 não pertencem à aquele mês.
+calendario_mes = calendar.monthcalendar(2024, 12)
+for dia in calendario_mes:
+    print(dia)
+# Os índices de cada um dos valores das listas, representarão o dia daquela respectiva semana.
+# No exemplo acima, o primeiro dia do mês começa em um domingo.
