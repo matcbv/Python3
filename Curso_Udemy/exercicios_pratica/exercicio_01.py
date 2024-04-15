82129c4d0fbceb7810c8d385c28ce37c256bc4e8
Matheus Cerqueira <matheuscbv23@gmail.com>
2024-04-15 11:05:56 -0300

    Atualizações

--- Curso_Udemy/exercicios_pratica/exercicio_01.py
+++ Curso_Udemy/exercicios_pratica/exercicio_01.py
@@ -1,3 +1,4 @@
+import sys
 from abc import ABC, abstractmethod
 from contextlib import contextmanager
 from functools import reduce, partial
@@ -18,10 +19,10 @@ class Opcoes(Enum):
 
 
 contador = count()
-
+caminho = Path().absolute() / 'banco_de_dados'
 
 @contextmanager
-def gerador_de_contexto(caminho, modo_abertura):
+def gerador_de_contexto(modo_abertura):
     print('Abrindo arquivo...')
     sleep(2)
     try:
@@ -48,48 +49,68 @@ def obter_horario():
 
 
 class ClasseQueGrava:
-    def __init__(self, msg, nome_arquivo):
+    def __init__(self, msg):
         self.msg = msg
-        self.nome_arquivo = nome_arquivo
+        self.caminho = caminho
 
     @abstractmethod
     def gravar_msg(self): ...
 
 
 class Email(ClasseQueGrava):
-    def __init__(self, msg: str, caminho: str, assunto: str, remetente: str, destinatario: str):
-        super().__init__(msg, caminho)
+    def __init__(self, msg: str, assunto: str, remetente: str, destinatario: str):
+        super().__init__(msg)
         self.assunto = assunto
         self.remetente = remetente
         self.destinatario = destinatario
 
     def gravar_msg(self):
-        caminho_arquivo = gerar_arquivo(self.nome_arquivo)
         hora = obter_horario()
-        with gerador_de_contexto(caminho_arquivo, 'a') as arquivo:
+        with gerador_de_contexto('a') as arquivo:
             arquivo.write(f'{self.msg} - {hora}')
 
 
 class SMS(ClasseQueGrava):
-    def __init__(self, msg: str, caminho: str, numero: int):
-        super().__init__(msg, caminho)
+    def __init__(self, msg: str, numero: int):
+        super().__init__(msg)
         self.numero = numero
 
     def gravar_msg(self):
-        caminho_arquivo = gerar_arquivo(self.nome_arquivo)
         hora = obter_horario()
-        with gerador_de_contexto(caminho_arquivo, 'a') as arquivo:
-            arquivo.write(f'{self.numero} - {self.msg} - {hora}')
+        with gerador_de_contexto('a') as arquivo:
+            arquivo.write(f'SMS - {self.numero} - {self.msg} - {hora}\n')
 
 
 class Ligacao(ClasseQueGrava):
-    def __init__(self, msg: str, caminho: str, numero: int):
-        super().__init__(msg, caminho)
+    def __init__(self, numero: int, msg: str = ''):
+        super().__init__(msg)
         self.numero = numero
 
     def gravar_msg(self):
-        caminho_arquivo = gerar_arquivo(self.nome_arquivo)
         hora = obter_horario()
-        with gerador_de_contexto(caminho_arquivo, 'a') as arquivo:
-            arquivo.write(f'{self.numero} - {self.msg} - {hora}')
+        with gerador_de_contexto('a') as arquivo:
+            arquivo.write(f'Ligação - {self.numero} - {hora}\n')
 
+
+enviar_msg = input('Deseja enviar uma mensagem? ').lower()[0]
+if enviar_msg == 'n':
+    sys.exit()
+print(f'1 - {Opcoes.opc01.value}\n'
+      f'2 - {Opcoes.opc02.value}\n'
+      f'3 - {Opcoes.opc03.value}\n')
+tipo_msg = input('Escolha uma das opções acima: ')
+
+match tipo_msg:
+    case 1:
+        remetente_ = input('Qual o seu email? ')
+        destinatario_ = input('Qual o email do destinatário? ')
+        assunto_ = input('Qual o assunto do email? ')
+        msg_ = input('Mensagem do email: ')
+        ligar = Email(msg_, assunto_, remetente_, destinatario_)
+    case 2:
+        numero_ = int(input('Para qual número deseja ligar? '))
+        msg_ = input('Mensagem do SMS: ')
+        ligar = SMS(msg_, numero_)
+    case 3:
+        numero_ = int(input('Para qual número deseja ligar? '))
+        ligar = Ligacao(numero_)
