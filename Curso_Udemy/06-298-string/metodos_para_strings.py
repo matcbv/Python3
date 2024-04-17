1b5be75ed50d22fca329f681f55b81447fbfa0f9
Matheus Cerqueira <matheuscbv23@gmail.com>
2024-04-17 17:57:53 -0300

    Atualização

---
+++ Curso_Udemy/06-298-string/metodos_para_strings.py
@@ -0,0 +1,29 @@
+import string
+
+# No módulo string, temos diversos métodos responsáveis por gerar conteúdos de texto.
+# Abaixo veremos os mais utilizados:
+
+# digits - Retorna os números de 0 a 9 (em formato de string)
+print(string.digits)
+print()
+
+# ascii_letters - Retorna as letras da sequência ascii.
+print(string.ascii_letters)
+print()
+
+# ascii_lowercase - Retorna somente as letras minúsculas da sequência ascii.
+print(string.ascii_lowercase)
+print()
+
+# ascii_uppercase - Retorna somente as letras maiúsculas da sequência ascii.
+print(string.ascii_lowercase)
+print()
+
+# punctuation - Retorna um conjunto de pontuações.
+print(string.punctuation)
+print()
+
+#  - Retorna todos os dígitos da sequência hexadecimal (letras minúsculas ou maiúsculas de A a F
+#  e os números de 0 a 9, totalizando 16 caracteres).
+print(string.hexdigits)
+print()
