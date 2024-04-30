275862836335f0202dd5daab61be103e99ff67c2
Matheus Cerqueira <matheuscbv23@gmail.com>
2024-04-30 18:59:14 -0300

    Update index.py

--- Curso_Udemy/exercicios_pratica/exercicio_06/index.py
+++ Curso_Udemy/exercicios_pratica/exercicio_06/index.py
@@ -3,15 +3,23 @@ from datetime import datetime
 from abc import ABC, abstractmethod
 from contextlib import contextmanager
 import json
-from string import Template
+import string
 from dotenv import load_dotenv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from smtplib import SMTP
 import os
+from dataclasses import dataclass
+import secrets
 
 load_dotenv()
 
-log_template = '$user logged at system $time'
+server_user = 'admin'
+server_password = None
+server = 'smtp.gmail.com'
+server_port = 587
+log_template = '$type sent by $user at $date'
+email_ = 'matheuscbv2334@gmail.com'
 
 
 def get_user_information():
@@ -26,12 +34,46 @@ def get_date():
     return current_date
 
 
+class Log(ABC):
+    def __init__(self):
+        self.msg_type = None
+        self.user = None
+        self.date = get_date()
+
+    @abstractmethod
+    def get_log(self): ...
+
+
+@dataclass
 class User:
-    def __init__(self, user, password):
-        self.user = password
-        self.password = password
+    user: str = None
+    password: str = None
+
+    def __post_init__(self):
+        self.user, self.password = get_user_information()
 
 
-class Email:
-    def send_log(self):
-        pass
+class Email(Log):
+
+    def get_log(self):
+        my_template = string.Template(log_template)
+        my_template = my_template.substitute(type='Email', user=self.user, date=self.date)
+        return my_template
+
+    @staticmethod
+    def get_email():
+        mime = MIMEMultipart()
+        mime['To'] = email_
+        mime['From'] = email_
+        mime['Subject'] = 'Documentação'
+
+        msg_body = MIMEText(log_template)
+        mime.attach(msg_body)
+        return mime
+
+    def send_email(self, my_email):
+        with SMTP(server, server_port) as email:
+            SMTP.starttls(email)
+            SMTP.ehlo(email)
+            SMTP.send_message(email, msg=my_email, from_addr=email_, to_addrs=email_)
+            SMTP.close(email)
