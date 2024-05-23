383537aaa246985450f1fd02f52d079f66683a7a
Matheus Cerqueira <matheuscbv23@gmail.com>
2024-05-23 02:54:11 -0300

    Atualizações

---
+++ Curso_Udemy/exercicios_pratica/exercicio_07/email_module.py
@@ -0,0 +1,51 @@
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from smtplib import SMTP
+from pathlib import Path
+from dotenv import load_dotenv
+from string import Template
+
+load_dotenv()
+
+# ENV DATA
+env = {'main_email': os.getenv('EMAIL'), 'main_email_password': os.getenv('EMAIL_PASSWORD')}
+
+# EMAIL SERVER DATA:
+esd = {'server': 'smtp.gmail.com', 'port': 587}
+
+# EMAIL TEMPLATE PATH
+email_template = Path().absolute() / 'email_template.html'
+
+
+class Email:
+    def __int__(self, client_name, client_lastname, email, password, subject, message):
+        self.client_name = client_name
+        self.client_lastname = client_lastname
+        self.email = email
+        self.password = password
+        self.subject = subject
+        self.message = message
+
+    def create_mime(self):
+        mime = MIMEMultipart()
+        mime['From'] = env['main_email_password']
+        mime['To'] = self.email
+        mime['Subject'] = self.subject
+        mime.attach(self.get_email_body())
+        return mime
+
+    def get_email_body(self):
+        email_html = Template(str(email_template))
+        email_html.substitute(addressee=self.client_name + self.client_lastname,
+                              content=self.message, remitter='M&M Bank')
+        email_body = MIMEText(str(email_html), _subtype='html', _charset='utf-8')
+        return email_body
+
+    def send_email(self):
+        with SMTP(self.email, esd['port'], timeout=15) as email:
+            email.ehlo()
+            email.starttls()
+            email.login(env['main_email'], env['main_email_password'])
+            email.send_message(self.create_mime())
+            email.close()
--- Curso_Udemy/exercicios_pratica/exercicio_07/index.py
+++ Curso_Udemy/exercicios_pratica/exercicio_07/index.py
@@ -1,38 +1,117 @@
-from functools import reduce
+from abc import ABC, abstractmethod
+from random import SystemRandom
+import string
+from pathlib import Path
+import email_module
 
-class Client:
-    def __init__(self, name, lastname, cpf, address):
-        self.name = name
-        self.lastname = lastname
-        self.cpf = cpf
-        self.address = address
+# LOGS PATH:
+logs_path = 'transitions_log.txt'
 
 
 def check_cpf(cpf, multiplier=10, i=0):
-    result = reduce(lambda acumulator, digit: acumulator + digit, cpf, 0)
-
+    counter = 0
     for d in cpf:
-        if not d.isdigit():
-            return 'CPF inválido!'
-        counter += int(d) * multiplier
-        multiplier -= multiplier
-    new_cpf +=
-    i += 1
-    if i > 1:
+        if not d.isdecimal():
+            return False
+        if multiplier > 1:
+            counter += int(d) * multiplier
+            multiplier -= 1
+    digit = get_digit(counter)
+    cpf += str(digit)
+    if i > 0:
+        if str(cpf)[-2:] == str(cpf)[-4:-2]:
+            return True
+        return False
+    return check_cpf(cpf, 11, 1)
+
+
+def get_digit(value):
+    if value % 11 > 2:
+        return 11 - (value % 11)
+    return 0
+
+
+def answer_validator(answer):
+    if answer not in 'sn':
+        print('Resposta inválida!')
+        return False
+    return True
+
+
+def get_new_password():
+    new_password = ''.join(SystemRandom().choices([string.ascii_letters, string.digits], k=8))
+    return new_password
+
+
+def transitions_logs():
+    pass
+
+
+class Client:
+    def __int__(self, name, lastname, age, cpf, address, salary):
+        self.name = name
+        self.lastname = lastname
+        self.age = age
+        self.cpf = cpf
+        self.address = address
+        self.salary = salary
+        self.bank_account = None
+
+
+class BankAccount(ABC):
+    def __init__(self, number_account, agency_number, retired=False, balance=0):
+        self.numberAccount = number_account
+        self.agencyNumber = agency_number
+        self.password = get_new_password()
+        self.retired = retired
+        self.balance = balance
+
+    def deposit(self, value):
+        self.balance += value
+        print('Depósito bem sucedido.\n'
+              f'Saldo atual: {self.balance}')
+        return True
+
+    @abstractmethod
+    def withdraw(self, value): ...
+
+
+class CurrentAccount(BankAccount):
+    def __int__(self, number_account, agency_number, retired, balance, overdraft):
+        super().__init__(number_account, agency_number, retired, balance)
+        self.overdraft = overdraft
+
+    def withdraw(self, value):
+        if value > self.balance:
+            print('Saldo indisponível para saque.')
+            active_overdraft = input('Deseja acionar seu cheque especial? [S/N] ').lower()
+            answer_validator(active_overdraft)
+            if not active_overdraft:
+                active_overdraft = input('Deseja acionar seu cheque especial? [S/N] ').lower()
+                answer_validator(active_overdraft)
+            if active_overdraft == 's':
+                if value > self.balance + self.overdraft:
+                    print('Sem limite disponível em seu cheque especial')
+                    return False
+                self.overdraft -= value - self.balance
+                self.balance = 0
+
+
+class SavingsAccount(BankAccount):
+    def withdraw(self, value):
+        if value > self.balance:
+            print('Saldo indisponível para saque.')
+            return False
+        self.balance -= value
+        print('Saque bem sucedido.\n'
+              f'Saldo atual: {self.balance}')
+        return True
+
+
+class CashMachine:
+    def __int__(self, account):
+        self.account = account
+
+    @property
+    def extract(self):
         return
-    return check_cpf(cpf + str(counter), multiplier=11)
-
-
-class Banco:
-    def __init__(self, numberAccount, agencyNumber):
-        self.numberAccount = numberAccount
-        self.agencyNumber = agencyNumber
-
-class Caixa:
-    def
-
-class Itau:
-    pass
-
-class Bradesco:
-    pass
