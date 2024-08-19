4d993422cf59a117d927fcdf6b9101ddb914dedf
matcbv <matheuscbv23@gmail.com>
2024-08-19 15:45:33 -0300

    Adicao de resumo

--- Curso_Udemy/complementos/conjuntos/metodos_para_conjuntos.py
+++ Curso_Udemy/complementos/conjuntos/metodos_para_conjuntos.py
@@ -30,7 +30,7 @@ set_01 = {1, 2, 3}
 set_02 = {3, 4, 5}
 intersection_set = set_01.intersection(set_02)
 print(intersection_set)
-# Obs.: Também podemos realizar a operação com o operador |. Ex.: ‘conjunto1’ | ‘conjunto2’
+# Obs.: Também podemos realizar a operação com o operador |. Ex.:
 intersection_set = set_01 | set_02
 print(intersection_set)
 
@@ -39,10 +39,23 @@ print(intersection_set)
 # Diferentemente do intersection, difference irá criar um novo conjunto com os elementos não existentes nos conjuntos.
 difference_set = set_01.difference(set_02)
 print(difference_set)
+# Obs.: Também podemos realizar a operação com o operador %. Ex.:
+intersection_set = set_01 - set_02
+print(intersection_set)
 
 # --------- set_01.symmetric_difference(set_02) ---------
 
 # Criará um terceiro conjunto contendo apenas os valores exclusivos entre dois outros conjuntos.
 symmetric_difference_set = set_01.symmetric_difference(set_02)
 print(symmetric_difference_set)
+# Obs.: Podemos realizar a operação com o operador ^. Ex.:
+symmetric_difference_set = set_01 ^ set_02
+print(symmetric_difference_set)
 
+# --------- set_01.symmetric_difference_update(set_02) ---------
+
+# Possui a mesma função do symmetric_update, porém, ao invés de criar um terceiro conjunto, irá alterar o primeiro.
+set_01.symmetric_difference_update(set_02)
+print(set_01)
+set_01 ^= set_02
+print(set_01)
