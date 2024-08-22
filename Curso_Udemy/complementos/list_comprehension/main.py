ab340e3b8c535213df453b06f72b1b33d3f26a11
matcbv <matheuscbv23@gmail.com>
2024-08-22 00:42:33 -0300

    Adicao de resumo

--- Curso_Udemy/complementos/list_comprehension/main.py
+++ Curso_Udemy/complementos/list_comprehension/main.py
@@ -9,18 +9,14 @@ print(lista_03)
 
 # Podemos chamar funções em nossas list comprehensions:
 
-# def factorial(num):
-#     answer = 1
-#     def do_fact():
-#         answer *= num
-#         if num > 1: factorial(num - 1)
-#     return answer
-#
-#
-# number_list = [1, 2, 3, 4, 5]
-# factorial_list = [factorial(num) for num in number_list]
-#
-# print(factorial_list)
+def factorial(num):
+    if num > 1: num *= factorial(num - 1)
+    return num
+
+number_list = [1, 2, 3, 4, 5]
+factorial_list = [factorial(num) for num in number_list]
+
+print(factorial_list)
 
 # Também podemos filtrar elementos:
 
@@ -28,3 +24,9 @@ number_list = [1, 2, 3, 4, 5]
 odd_list = [num for num in number_list if num % 2 != 0]
 
 print(odd_list)
+
+# Por fim, podemos utilizar o operador := para capturar o valor de uma expressão e reutilizá-lo:
+
+n = 0
+results = [(n := x ** 2) for x in range(10) if n > 20]
+print(results)
