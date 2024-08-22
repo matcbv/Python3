a4191d528e0f7ffbe8788e4ad1a74ebc3b945c30
matcbv <matheuscbv23@gmail.com>
2024-08-22 00:21:50 -0300

    Adicao de resumo

--- Curso_Udemy/complementos/list_comprehension/main.py
+++ Curso_Udemy/complementos/list_comprehension/main.py
@@ -7,4 +7,24 @@ lista_02 = ['a', 'b', 'c']
 lista_03 = [[x, y] for x in lista_01 for y in lista_01]
 print(lista_03)
 
-# Podemos também chamar funções em nossas list comprehensions
+# Podemos chamar funções em nossas list comprehensions:
+
+# def factorial(num):
+#     answer = 1
+#     def do_fact():
+#         answer *= num
+#         if num > 1: factorial(num - 1)
+#     return answer
+#
+#
+# number_list = [1, 2, 3, 4, 5]
+# factorial_list = [factorial(num) for num in number_list]
+#
+# print(factorial_list)
+
+# Também podemos filtrar elementos:
+
+number_list = [1, 2, 3, 4, 5]
+odd_list = [num for num in number_list if num % 2 != 0]
+
+print(odd_list)
